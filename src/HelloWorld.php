3d2dd69555a3e1a9bfe925a05ca080d90ee32a23
Roman Braciník <fejjo@igloonet.cz>
2017-11-25 12:35:57 +0100

    oprava

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,5 +4,14 @@ namespace PosledniSobota;
 
 class HelloWorld
 {
-
+	private $name;
+	
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+	public function sayHello()
+	{
+		return 'Hello ' . $this->name;
+	}
 }
