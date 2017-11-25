13415caaa5b27619eda8e9c56b65be97471e4f19
Roman Braciník <fejjo@igloonet.cz>
2017-11-25 12:31:42 +0100

    failujuci test

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -5,8 +5,9 @@ use PosledniSobota\HelloWorld;
 
 class HelloWorldTest extends \PHPUnit\Framework\TestCase
 {
-    public function testHelloWorld()
-    {
-        $helloWorld = new HelloWorld();
-    }
+	public function testHelloWorld()
+	{
+		$helloWorld = new HelloWorld('myName');
+		$this->assertEquals('Hello myName', $helloWorld->sayHello());
+	}
 }
