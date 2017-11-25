1ec3928cb5c6fb7fcbfddb451e76c60e38892296
Roman Braciník <fejjo@igloonet.cz>
2017-11-25 11:27:45 +0100

    pridanie code stylu

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,10 +1,12 @@
 <?php
 namespace PosledniSobota\Tests;
+
 use PosledniSobota\HelloWorld;
+
 class HelloWorldTest extends \PHPUnit\Framework\TestCase
 {
-	public function testHelloWorld()
-	{
-		$helloWorld = new HelloWorld();
-	}
+    public function testHelloWorld()
+    {
+        $helloWorld = new HelloWorld();
+    }
 }
